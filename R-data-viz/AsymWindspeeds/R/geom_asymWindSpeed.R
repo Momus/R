95cc0ada02fe09ece2f467cedd4b110d11497c92
Dmitri G. Brengauz <dmitri@momus.net>
2017-05-19 16:48:04 -0600

    Cleaning pretty much done.

--- R-data-viz/AsymWindspeeds/R/geom_asymWindSpeed.R
+++ R-data-viz/AsymWindspeeds/R/geom_asymWindSpeed.R
@@ -1,6 +1,7 @@
 #' Maping wind speeds of a hurricane.
-library(tidyvese)
+library(tidyverse)
 library(lubridate)
+library(data.table)
 
 #' Download the dataset
 #' http://rammb.cira.colostate.edu/research/tropical_cyclones/tc_extended_best_track_dataset/
@@ -53,19 +54,48 @@ caine_names <- c("name",
 #Ike <- filter(firstCaine, name == "IKE", year == "2008")
 
 
-Ike <- read_table2("./data/Ike.txt",
+Ike <- read_table2("../data/Ike.txt",
                   col_names = caine_names,
                   col_types = caine_columns)
 
 #This didn't generate any NA values
 
+datedIke <- Ike %>%
+    mutate(date = ymd_hm(paste(year, "/",substring(MDT,1,2), "/" ,
+                               substring(MDT,3,4), "/",
+                               substring(MDT,5,6), "00", sep="")),
+           longitude = -(longitude_W)) %>%
+    unite("storm_id",
+          c(name, year),
+          sep="-")
 
-mutate(Ike,
-       date = ymd_hm( paste(year,
-                            "/",
-                            substring(MDT,1,2),
-                            "/" ,
-                            substring(MDT,3,4),
-                            "/", substring(MDT,5,6),
-                            "00",
-                            sep="")))
+##If I wanted to write a more generalized function one day, I could
+##use: x <- function(y){print(match.call())[2]}
+
+
+NEIke <- select(datedIke, storm_id, date, latitude, longitude,
+                `34` = `34NE`, `50` = `50NE`,  `64` = `64NE`) %>%
+    gather(`34`, `50`, `64`, key="wind_speed", value="NE")
+
+
+SEIke <- select(datedIke, storm_id, date, latitude, longitude,
+               `34` = `34SE`, `50` = `50SE`, `64` = `64SE`) %>%
+    gather(`34`, `50`, `64`, key="wind_speed", value="SE")
+
+
+NWIke <- select(datedIke, storm_id, date, latitude, longitude,
+               `34` = `34NW`, `50` = `50NW`, `64` = `64NW`) %>%
+    gather(`34`, `50`, `64`, key="wind_speed", value="NW")
+
+
+SWIke <- select(datedIke, storm_id, date, latitude, longitude,
+               `34` = `34SW`, `50` = `50SW`, `64` = `64SW`) %>%
+    gather(`34`, `50`, `64`, key="wind_speed", value="SW")
+
+
+cleanIke <- NEIke %>%
+    full_join(NWIke) %>%
+    full_join(SEIke) %>%
+    full_join(SWIke)
+
+
